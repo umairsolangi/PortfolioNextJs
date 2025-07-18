c0b4359f58faee30d5f559431e84e5213dd29529
umairsolangi <solangitv@gmail.com>
2025-07-18 21:37:40 +0500

    Changing file name form jsx to tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
       </div>
       <main className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800">
         <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <p>I&apos;m a CS student currently learning React.js and Next.js. I'm passionate about frontend development and building clean UIs.</p>
+        <p>I am a CS student currently learning React.js and Next.js. I'm passionate about frontend development and building clean UIs.</p>
         <h3 className="text-xl font-semibold mt-4">Technologies I use:</h3>
         <ul className="list-disc ml-6">
           <li>HTML</li>
